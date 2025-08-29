49ea0b906417392f628a8b8b7990019777360b38
Carolyn Wilkinson <carolynwilkinson94@gmail.com>
2025-08-29 12:12:09 +0800

    added pledge permissions

--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -1,11 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from django.http import Http404
 from .models import Fundraiser, Pledge
 from .serializers import FundraiserSerializer, PledgeSerializer, FundraiserDetailSerializer
+from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnlyCustom
 
 class FundraiserList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         fundraisers = Fundraiser.objects.all()
@@ -15,7 +18,7 @@ class FundraiserList(APIView):
     def post(self, request):
         serializer = FundraiserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
@@ -26,9 +29,15 @@ class FundraiserList(APIView):
         )
     
 class FundraiserDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, 
+        IsOwnerOrReadOnly
+    ]
+
     def get_object(self, pk):
         try:
             fundraiser = Fundraiser.objects.get(pk=pk)
+            self.check_object_permissions(self.request, fundraiser)
             return fundraiser
         except Fundraiser.DoesNotExist:
             raise Http404
@@ -38,8 +47,28 @@ class FundraiserDetail(APIView):
         serializer = FundraiserDetailSerializer(fundraiser)
         return Response(serializer.data)
     
+    def put(self, request, pk):
+        fundraiser = self.get_object(pk)
+        serializer = FundraiserDetailSerializer(
+            instance=fundraiser,
+            data=request.data,
+            partial=True
+
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    
     
 class PledgeList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         pledges = Pledge.objects.all()
         serializer = PledgeSerializer(pledges, many=True)
@@ -48,7 +77,7 @@ class PledgeList(APIView):
     def post(self, request):
         serializer = PledgeSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(supporter=request.user)
             return Response(
                 serializer.data, 
                 status=status.HTTP_201_CREATED
@@ -58,3 +87,33 @@ class PledgeList(APIView):
                 serializer.errors, 
                 status=status.HTTP_400_BAD_REQUEST
             )
+        
+
+class PledgeDetail(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnlyCustom]
+
+    def get_object(self, pk):
+        try:
+            pledge = Pledge.objects.get(pk=pk)
+            self.check_object_permissions(self.request,pledge)
+            return pledge
+        except Pledge.DoesNotExist:
+            raise Http404
+        
+    def get(self,request,pk):
+        pledge = self.get_object(pk)
+        serializer = PledgeSerializer(pledge)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        pledge = self.get_object(pk)
+        serializer = PledgeSerializer(pledge, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        pledge = self.get_object(pk)
+        pledge.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
